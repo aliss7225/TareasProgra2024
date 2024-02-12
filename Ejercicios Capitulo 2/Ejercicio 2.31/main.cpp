6497f0aacf40102a7e22091c07bb6a8c7cb1fc1b
Alisson López <156858728+aliss7225@users.noreply.github.com>
2024-02-12 15:57:59 -0600

    Update main.cpp

--- Ejercicios Capitulo 2/Ejercicio 2.31/main.cpp
+++ Ejercicios Capitulo 2/Ejercicio 2.31/main.cpp
@@ -4,6 +4,31 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    float kilometros_dia,precio_gasolina,litro_gasolina,parqueo,peaje=0;
+
+    float Costo_gasolina,Costo_total_gasolina,Total;
+
+
+    cout<<"Ingrese la cantidad de kilometros que recorrio durante el día"<<endl;
+    cin>>kilometros_dia;
+
+    cout<<"Ingrese el precio pagado por el litro de gasolina"<<endl;
+    cin>>precio_gasolina;
+
+    cout<<"Segun su vehiculo ingrese cual es el promedio de gasolina que gasta por kilometro"<<endl;
+    cin>>litro_gasolina;
+
+    cout<<"Ingrese cuanto pago por el estacionamiento" << endl;
+    cin >> parqueo;
+
+    cout<<"ingrese cuanto pago por el peaje del día de hoy" << endl;
+    cin>>peaje;
+
+
+    Costo_gasolina   =(precio_gasolina)/(kilometros_dia);
+    Costo_total_gasolina       =(Costo_gasolina)*(kilometros_dia);
+    Total                      =(Costo_total_gasolina)+(parqueo)+(peaje);
+    cout<<"el costo diario por conduicr "<<kilometros_dia<<" KM es "<<Total<< " Quetzales al dia"<<endl;
+
     return 0;
 }
