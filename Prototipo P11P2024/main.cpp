949caa84cf648c9f34f220ca19ee445d41e0cf78
Alisson López <156858728+aliss7225@users.noreply.github.com>
2024-02-28 10:03:40 -0600

    Add files via upload

---
+++ Prototipo P11P2024/main.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+#define NUMERO_ALUMNOS 5
+#define NUMERO_MATERIAS 5
+#define NUMERO_NOTAS 4
+#define MAX_CALIFICACION 100
+#define MIN_CALIFICACION 0
+#define MAXIMA_LONGITUD_CADENA 100
+
+void llamaCiclo();
+int busquedaAleatorios(int minimo, int maximo);
+void llenarMatriz(float matriz[NUMERO_ALUMNOS][NUMERO_NOTAS + 1]);
+void imprimirMatrizLinea();
+float imprimirMatriz(float matriz[NUMERO_ALUMNOS][NUMERO_NOTAS + 1], char alumnos[NUMERO_ALUMNOS][MAXIMA_LONGITUD_CADENA], string nombreFacultad);
+int main()
